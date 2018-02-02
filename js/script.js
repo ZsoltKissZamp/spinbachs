478e3d528074c051a717661f9399a3216906e6b9
Kiss Zsolt <dzsingis@gmail.com>
2018-02-02 23:53:44 +0100

    b1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let h3 = document.getElementById('change');
 let change = setInterval(function() {
   count++;
   if(count == 13) {
-    h3.innerHTML = "Thank You 4 Da Visit! See you Soon!";
+    h3.innerHTML = "Thank You 4 the Drop by! See you Soon!";
     clearInterval(change);
   }
 }, 1000);
